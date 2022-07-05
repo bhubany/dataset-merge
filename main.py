ef9514c9bf7110ee8f05272242998405ed753c19
bhubany <yadav.bhuban.by@gmail.com>
2022-07-06 01:22:56 +0545

    Dataset merge completed

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
-from textwrap import indent
 import json
 
-header=[]
-parsed_data = []
-temp =""
-temp2 = ""
-temp3 = [] 
 
-with open("sample.txt" ,'r',) as f1:
-    line = f1.read().splitlines()
-    for i in range(0,5):
-        temp = temp + line[i].strip()
+with open ("sample.txt" , "r") as f1:
+    data = f1.read().strip().splitlines()
+
+merged_data=[]
+i=0
+dict ={}
+
+while i in range(0, len(data)):
+    if data[i] != " ":
+        dict[data[i].split("|")[0]] = data[i].split("|")[1]
     
-    for i in  range(0, len(temp.split(' '))):
-        if(temp.split(' ')[i] != "" and temp.split(' ')[i] not in temp3):
-            header.append(temp.split(' ')[i])
-
-    for j in range(5, len(line)):
-        for item in line[j].split(" "):
-            if(item != "" and not(item.isnumeric()) and not(item.isupper())):
-                temp2 = temp2 +" "+item
-            elif(item != "" and not(item.isnumeric()) and (item.isupper())):
-                temp3.append(temp2)
-                temp3.append(item)
-                temp2=""
-            elif (item != "" and (item.isnumeric()) and not(item.isupper())):
-                temp3.append(item)
-
-
-k = 0
-while(k<len(temp3)):
-    dict={}
-    dict[header[k%3]] = temp3[k]
-    dict[header[k%3 +1]] = temp3[k +1]
-    dict[header[k%3 +2]] = temp3[k +2]
-    parsed_data.append(dict)
-    k+=3
+    if(i%4 == 0 and i != 0):
+        merged_data.append(dict)
+        dict={}
+    i+=1
 
 with open("output.json", "w") as file:
-    json.dump(parsed_data, file ,indent=2)
+    json.dump(merged_data, file , indent=2)
