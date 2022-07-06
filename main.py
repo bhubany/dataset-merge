7683f10d962aab81b1dca5fb47fdf86af63e8c09
bhubany <yadav.bhuban.by@gmail.com>
2022-07-06 22:48:05 +0545

    Dataset -merge completed

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-import json
+import csv
 
+word_list = ['district','Kathmandu','Dhanusha','Kavrepalanchowk']
+def spellingCorrector(word, word_list = word_list):
+    word = word.replace(" ", "")
+    for w in word_list:
+        if(w == word):
+            return w
+        else:
+            count = 0
+            length =len(w) if(len(w) < len(word)) else len(word)
+            for i in range(0, length):
+                if(w[i] == word[i]):
+                    count+=1
+
+            if (count >= len(word)/2):
+                return w
 
 with open ("sample.txt" , "r") as f1:
     data = f1.read().strip().splitlines()
 
-merged_data=[]
-header = []
-col1=[]
+
+dict1={}
+dict2={}
 i=0
 
 while i in range(0, len(data)):
     if (data[i] != " " and i <= 4):
-        header.append(data[i].split("|")[0])
-        col1.append(data[i].split("|")[1])
+        dict1[spellingCorrector(data[i].split("|")[0])] =data[i].split("|")[1]
 
     elif (data[i] != " " and i > 4):
-        col1.append(data[i].split("|")[1])
-
-    if(i==4):
-        merged_data.append(header)
-
-    if(i%4 == 0 and i != 0):
-        merged_data.append(col1)
-        col1=[]
+        dict2[spellingCorrector(data[i].split("|")[0])] =data[i].split("|")[1]
     i+=1
 
-with open("output.json", "w") as file:
-    json.dump(merged_data, file , indent=2)
+f = open('output.csv', 'w')
+writer = csv.writer(f)
+
+for key in dict1:
+    list1 = [key , dict1[key], dict2[key]]
+    writer.writerow(list1)
